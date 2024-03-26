b319875d9a624712c8aab4728ac211c72f96ff6f
Henrique Guerra Messias <hgmessias@hotmail.com>
2024-03-25 21:01:30 -0300

    Finalização do Mobile

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,13 @@
-const headerlinks = document.querySelector(".header-style__links");
-document.getElementById("handlerNavbar") .addEventListener("click", () => {
-    headerlinks.classList.toggle ("open")
+const nav = document.querySelector(".header-style__links");
+const btnNav = document.getElementById("handlerNavebar");
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    document.querySelector(".header-style__links").classList.add("hidden");
+    document.getElementById("handlerNavebar").addEventListener("click",function(){
+    document.querySelector(".header-style__links").classList.remove("hidden");
+    btnNav.addEventListener("click", () => {
+        nav.classList.toggle("open")
+    })    
+    })
 })
